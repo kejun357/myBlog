4bfbc6e7d9981db292bea0721f4d4d91452f41af
kejun357 <527343714@qq.com>
2020-01-03 18:27:03 +0800

    'czy'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var arr = [{
+    title: '标题一',
+    content: '这里是内容'
+  }, {
+    title: '标题一',
+    content: '这里是内容'
+  }, {
+    title: '标题一',
+    content: '这里是内容'
+  }, {
+    title: '标题一',
+    content: '这里是内容'
+  }, {
+    title: '标题一',
+    content: '这里是内容'
+  }]
+  res.render('index', { list: arr, title: 'Express', say: '我这心<a href="">里卧槽</a>卧槽的' , answer: '<b>嗯嗯</b>'});
 });
 
 module.exports = router;
