4bda4b2663489c35f789dd1401311bef1fc6a35b
kejun357 <527343714@qq.com>
2020-01-03 16:08:22 +0800

    ''

--- app.js
+++ app.js
@@ -9,11 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(express.static('public'))
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
