62aab15f122241370192bfbdcdf58f8db9f37b62
kejun357 <527343714@qq.com>
2020-01-07 18:40:06 +0800

    ''

---
+++ controller/blog.js
@@ -0,0 +1,43 @@
+const getList = (name) => {
+    return [{
+        id: '第一章',
+        title: '被驱逐的高手'
+    }, {
+        id: '第二章',
+        title: 'C区47号'
+    }, {
+        id: '第三章',
+        title: '专职夜班'
+    }, {
+        id: '第四章',
+        title: '神秘高手'
+    }, {
+        id: '第五章',
+        title: '技能搭配'
+    }, {
+        id: '第六章',
+        title: '千机伞'
+    }, {
+        id: '第七章',
+        title: '暗夜猫妖'
+    }, {
+        id: '第八章',
+        title: '鱼死网破'
+    }, {
+        id: '第九章',
+        title: '叶修的双手'
+    }, {
+        id: '第十章',
+        title: '无耻之徒'
+    }, {
+        id: '第十一章',
+        title: '差人加一个'
+    }, {
+        id: '第十二章',
+        title: '蜘蛛洞穴'
+    }]
+}
+
+module.exports = {
+    getList
+}
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { getList } = require('../controller/blog')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var arr = [{
-    title: '标题一',
-    content: '这里是内容'
-  }, {
-    title: '标题一',
-    content: '这里是内容'
-  }, {
-    title: '标题一',
-    content: '这里是内容'
-  }, {
-    title: '标题一',
-    content: '这里是内容'
-  }, {
-    title: '标题一',
-    content: '这里是内容'
-  }]
-  res.render('index', { list: arr, title: 'Express', say: '我这心<a href="">里卧槽</a>卧槽的' , answer: '<b>嗯嗯</b>'});
+  var arr = getList()
+  console.log(arr)
+  res.render('index', { list: arr });
 });
 
 module.exports = router;
